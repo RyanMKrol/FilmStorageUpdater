293eb38b6508fd100d66903a82ff0afe62d1d150
Ryan Krol <ryankr@hotmail.co.uk>
2020-02-22 12:00:39 +0000

    Setup base cronjob for project
    * This will be responsible for scheduling the data
    update

--- src/app.js
+++ src/app.js
@@ -1,37 +1,6 @@
 // app.js
-import express from 'express'
-import path from 'path'
-import cookieParser from 'cookie-parser'
-import logger from 'morgan'
-import createError from 'http-errors'
-import cors from "cors"
+import schedule from 'node-schedule'
 
-import exampleRouter from './routes/example'
-
-const app = express()
-
-app.use(cors())
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-
-app.use('/example/endpoint', exampleRouter)
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404))
-})
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.send('500 - Internal Server Error')
-})
-
-export default app
+schedule.scheduleJob('* * * * * *', function(){
+  console.log('The answer to life, the universe, and everything!');
+});
