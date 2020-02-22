6425824b37296237fc3c8161d6539c1349f4d5e8
Ryan Krol <ryankr@hotmail.co.uk>
2020-02-22 14:27:21 +0000

    Tie all data together in scheduled job
    * Everything should be running once a month now!

--- src/app.js
+++ src/app.js
@@ -1,6 +1,36 @@
 // app.js
 import schedule from 'node-schedule'
+import {
+  downloadMoviesList,
+  uploadMoviesData,
+} from './api/S3'
+import fetchDataByName from './api/OMDB'
+import sendMail from './api/Email'
 
-schedule.scheduleJob('* * * * * *', function(){
-  console.log('The answer to life, the universe, and everything!');
-});
+schedule.scheduleJob('0 0 0 1 * *', async () => {
+  try {
+    const movieList = await downloadMoviesList()
+    const dataArray = movieList.split(/\r?\n/)
+
+    const noMetadataDataArray = dataArray.map((item) => {
+      return item.replace(/ ?\(.*/, "")
+    })
+
+    const movieDataRequests = noMetadataDataArray.map((movieName) => {
+      return fetchDataByName(movieName)
+    })
+
+    const movieData = await Promise.all(movieDataRequests)
+
+    const filteredMovieData = movieData.filter((item) => {
+      return item.Response === 'True'
+    })
+
+    await uploadMoviesData(filteredMovieData)
+
+    sendMail('Updated your movie list!', JSON.stringify(movieData))
+  } catch(error) {
+    console.log(error)
+    sendMail('Failed to update film storage!', error.toString())
+  }
+})
