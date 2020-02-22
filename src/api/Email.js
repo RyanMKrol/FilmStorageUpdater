8f6f06e5dfd34c44a7c41c3e4927358e89cf5757
Ryan Krol <ryankr@hotmail.co.uk>
2020-02-22 14:27:06 +0000

    Add upload method to S3 API module

--- src/api/Email.js
+++ src/api/Email.js
@@ -1,9 +1,9 @@
-import nodemailer from "nodemailer"
+import nodemailer from 'nodemailer'
 import gmailCredentials from './../../credentials/GmailCredentials.json'
 
 async function sendMail(subject, body) {
   let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: 'smtp.gmail.com',
     port: 587,
     secure: false,
     auth: gmailCredentials
@@ -11,12 +11,12 @@ async function sendMail(subject, body) {
 
   let info = await transporter.sendMail({
     from: '"FilmStorageUpdater" <ryankrol.m@gmail.com>',
-    to: "ryankrol.m@gmail.com",
+    to: 'ryankrol.m@gmail.com',
     subject: subject,
     text: body,
   })
 
-  console.log("Message sent: %s", info.messageId)
+  console.log('Message sent: %s', info.messageId)
 }
 
 export default sendMail
--- src/api/S3.js
+++ src/api/S3.js
@@ -3,6 +3,7 @@ import s3credentials from './../../credentials/S3Credentials.json'
 
 const MOVIE_BUCKET_NAME = 'ryankrol-films'
 const MOVIE_FILE_KEY = 'films.txt'
+const MOVIE_DATA_FILE_KEY = 'films_data.txt'
 
 const downloadMoviesList = () => {
   AWS.config.update(s3credentials)
@@ -20,4 +21,25 @@ const downloadMoviesList = () => {
   })
 }
 
-export default downloadMoviesList
+const uploadMoviesData = (data) => {
+  AWS.config.update(s3credentials)
+
+  const params = {
+    Bucket: MOVIE_BUCKET_NAME,
+    Key: MOVIE_DATA_FILE_KEY,
+    Body: JSON.stringify(data)
+  }
+
+  return new Promise((resolve, reject) => {
+    new AWS.S3().upload(params, (error, data) => {
+      if (error) reject(error)
+      console.log(data)
+      resolve('Success!')
+    })
+  })
+}
+
+export {
+  downloadMoviesList,
+  uploadMoviesData,
+}
