481786980b5764acf043183a4e67836ba0d51de7
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-15 22:00:13 +0100

    Update version of bable and noodle-email

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 // app.js
 import schedule from 'node-schedule'
-import {
-  downloadMoviesList,
-  uploadMoviesData,
-} from './api/S3'
+import { downloadMoviesList, uploadMoviesData } from './api/S3'
 import fetchDataByName from './api/OMDB'
-import MailSender from 'noodlemail'
+import MailSender from 'noodle-email'
 import gmailCredentials from './../credentials/GmailCredentials.json'
 
 const mailClient = new MailSender(gmailCredentials)
@@ -17,24 +14,24 @@ schedule.scheduleJob('0 0 0 1 * *', async () => {
     const movieList = await downloadMoviesList()
     const dataArray = movieList.split(/\r?\n/)
 
-    const noMetadataDataArray = dataArray.map((item) => {
-      return item.replace(/ ?\(.*/, "")
+    const noMetadataDataArray = dataArray.map(item => {
+      return item.replace(/ ?\(.*/, '')
     })
 
-    const movieDataRequests = noMetadataDataArray.map((movieName) => {
+    const movieDataRequests = noMetadataDataArray.map(movieName => {
       return fetchDataByName(movieName)
     })
 
     const movieData = await Promise.all(movieDataRequests)
 
-    const filteredMovieData = movieData.filter((item) => {
+    const filteredMovieData = movieData.filter(item => {
       return item && item.Response === 'True'
     })
 
     await uploadMoviesData(filteredMovieData)
 
     mailClient.sendMail('Updated your movie list', JSON.stringify(movieData))
-  } catch(error) {
+  } catch (error) {
     console.log(error)
     mailClient.sendMail('Failed to update film storage', error.toString())
   }
